7e01c3726c939e71cce915c8ccc432917301a1ff
Mubashir <mubashirahmmed902@gmail.com>
2026-05-20 18:44:45 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import{DB_Name}from "../constants.js"
+const connectDB = async()=>{
+   try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
+    console.log(`\n MONGO_DB si connected !! DB Host {connectionInstance.connection.host}`);
+    
+   } catch (error) {
+    console.log("Mongo-DB ERROR",error);
+    process.exit(1)
+   }
+    
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env"
+});
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+// const app = express()
+// ;(async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
+//         app.on("ERROR", (error)=>{
+//             console.log("ERROR",error);
+//             throw err    
+//         });
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`app is listening on port ${process.env.PORT}`);
+            
+//         })
+        
+//     } catch (error) {
+//         console.log(error);
+//         throw err
+//     }
+// })()
